643070d4235f02d8733387c78a7cb763cf65e3e3
lsshawn <coffee.or.tea88@gmail.com>
2020-03-25 15:19:50 +1300

    show folders with  util

---
+++ src/components/FolderTree.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <q-tree
+      ref="folders"
+      @lazy-load="lazyLoad"
+      :nodes="rootDir"
+      node-key="nodeKey"
+    />
+  </div>
+</template>
+
+<script>
+const path = require("path");
+
+export default {
+  props: {
+    rootDir: {
+      type: Array,
+      required: true
+    },
+    folder: {
+      type: String,
+      required: false
+    },
+    lazyLoad: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selected: null,
+      isWin: process.platform === "win32"
+    };
+  }
+};
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <folder-tree
+        :rootDir="rootDir"
+        :folder.sync="selectedFolder"
+        :lazyLoad="onLazyLoad"
+      />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import FolderTree from "components/FolderTree";
+
+import walkFolders from "../utils/walkFolders";
+const path = require("path");
+const fs = require("fs");
+const mime = require("mime-types");
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    FolderTree
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      rootDir: [],
+      selectedFolder: null,
+      drive: process.platform === "win32" ? "C:" : ""
+    };
+  },
+  methods: {
+    getFolders(absolutePath) {
+      let folders = [];
+
+      if (!absolutePath || typeof absolutePath !== "string") {
+        return folders;
+      }
+
+      for (const fileInfo of walkFolders(absolutePath, 0)) {
+        // we only want folders
+        if (!fileInfo.isDir) {
+          continue;
+        }
+
+        // create new node
+        const node = this.createNode(fileInfo);
+        folders.push(node);
+      }
+
+      return folders;
+    },
+    setSelectedFolder(folder) {
+      this.selectedFolder = folder;
+      // handle windows drive
+      if (process.platform === "win32") {
+        if (
+          this.selectedFolder.charAt(this.selectedFolder.length - 1) === ":"
+        ) {
+          this.selectedFolder += path.sep;
+        }
+      }
+    },
+    createNode(fileInfo) {
+      let nodeKey = fileInfo.rootDir;
+      if (nodeKey.charAt(nodeKey.length - 1) !== path.sep) {
+        nodeKey += path.sep;
+      }
+      if (fileInfo.fileName === path.sep) {
+        fileInfo.fileName = nodeKey;
+      } else {
+        nodeKey += fileInfo.fileName;
+      }
+      // get file mime type
+      const mimeType = mime.lookup(nodeKey);
+      // create object
+      return {
+        label: fileInfo.fileName,
+        nodeKey: nodeKey,
+        expandable: fileInfo.isDir,
+        tickable: true,
+        lazy: true,
+        children: [],
+        data: {
+          rootDir: fileInfo.rootDir,
+          isDir: fileInfo.isDir,
+          mimeType: mimeType,
+          stat: fileInfo.stat
+        }
+      };
+    },
+    onLazyLoad: () => {}
+  },
+  created() {
+    if (process.platform === "win32") {
+    } else {
+      // set and get root folder's folders
+      this.setSelectedFolder(this.drive + path.sep);
+      this.rootDir.push(...this.getFolders(this.selectedFolder));
+    }
+  }
+};
+</script>
